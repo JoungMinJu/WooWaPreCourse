214099eaf53b5d1a2e5d06732a7be6b9dc12cdab
MINJU <alswn9938@gmail.com>
2022-12-04 21:56:32 +0900

    test : 각 페이지의 각 자리 숫자를 모두 곱하기

--- src/test/java/onboarding/problem1/model/BookTest.java
+++ src/test/java/onboarding/problem1/model/BookTest.java
@@ -24,12 +24,33 @@ class BookTest {
                 );
     }
 
+
+    static Stream<Arguments> generateSecondData(){
+        return Stream.of(
+                Arguments.of(Arrays.asList(35,36), 0, 15),
+                Arguments.of(Arrays.asList(123,124), 1, 8),
+                Arguments.of(Arrays.asList(233,234), 0, 18),
+                Arguments.of(Arrays.asList(335,336), 1, 54)
+        );
+    }
+
+
+
     @ParameterizedTest(name = "{0}의 {1} 번째 숫자의 각자리 합은 {2}")
     @MethodSource("generateData")
     @DisplayName("각 자리 합 구하는 로직")
     void 각자리합(List<Integer> lst, int index, int result){
         Book book = new Book(lst);
         Assertions.assertThat(book.getSumScore(index)).isEqualTo(result);
+    }
+
+
+    @ParameterizedTest(name = "{0}의 {1} 번째 숫자의 각자리 곱은 {2}")
+    @MethodSource("generateSecondData")
+    @DisplayName("각 자리 곱 구하는 로직")
+    void 각자리곱(List<Integer> lst, int index, int result){
+        Book book = new Book(lst);
+        Assertions.assertThat(book.getMultiScore(index)).isEqualTo(result);
     }
 
 }
