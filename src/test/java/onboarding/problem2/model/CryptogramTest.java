69730ac1ec10f09fed89e5e03440062d5c1ff9a8
MINJU <alswn9938@gmail.com>
2022-12-05 15:43:01 +0900

    (Q2) test : 중복 문자 제거 기능

--- src/test/java/onboarding/problem2/model/CryptogramTest.java
+++ src/test/java/onboarding/problem2/model/CryptogramTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.stream.Stream;
 
 class CryptogramTest {
@@ -18,6 +21,21 @@ class CryptogramTest {
         return Stream.of(
                 Arguments.of("abcd",  new LinkedList<>(Arrays.asList('a','b','c','d'))),
                 Arguments.of("bbcde", new LinkedList<>(Arrays.asList('b','b','c','d','e')))
+        );
+    }
+
+    static Stream<Arguments> getSecondData(){
+        return Stream.of(
+                Arguments.of("aaabcbaa", new LinkedList<>(Arrays.asList('b','c','b'))),
+                Arguments.of("abcd", new LinkedList<>(Arrays.asList('a','b','c','d'))),
+                Arguments.of("bcddaeef", new LinkedList<>(Arrays.asList('b','c','a','f')))
+        );
+    }
+
+    static Stream<Arguments> getThirdData(){
+        return Stream.of(
+                Arguments.of("abcd", new LinkedList<>(Arrays.asList('a','b','c','d')), true),
+                Arguments.of("bcddaeef", new LinkedList<>(Arrays.asList('b','c','a','f')),false)
         );
     }
 
@@ -52,6 +70,24 @@ class CryptogramTest {
         Cryptogram cryptogram = new Cryptogram(input);
         Assertions.assertThat(cryptogram.getCryptoCharacters())
                 .isEqualTo(result);
+    }
+
+    @ParameterizedTest(name= "{0}이라는 암호문의 중복 문자열을 한 번 지운 결과는 {1}")
+    @MethodSource("getSecondData")
+    @DisplayName("중복 문자열 지우기")
+    void 중복_문자열(String input, LinkedList<Character> expect){
+        Cryptogram cryptogram = new Cryptogram(input);
+        cryptogram.updateCryptogram();
+        Queue<Character> resultCharacters = cryptogram.getCryptoCharacters();
+        Assertions.assertThat(resultCharacters).isEqualTo(expect);
+    }
+
+    @ParameterizedTest(name = "{0}이라는 암호문의 사이즈가 {1}의 사이즈와 일치하는지의 결과는 {2}")
+    @MethodSource("getThirdData")
+    @DisplayName("큐 사이즈 검증")
+    void 큐_사이즈_검증(String input, LinkedList<Character> lst, boolean result){
+        Cryptogram cryptogram = new Cryptogram(input);
+        Assertions.assertThat(cryptogram.isSameSize(lst)).isEqualTo(result);
     }
 
 
