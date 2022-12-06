1c4c9f3ad7af9ef7b1a634ffb00b20a65a408de3
alswn <alswn9938@gmail.com>
2022-12-06 14:21:09 +0900

    add : 최종 로직

--- src/main/java/onboarding/problem3/Problem3.java
+++ src/main/java/onboarding/problem3/Problem3.java
@@ -6,6 +6,6 @@ public class Problem3 {
 
     public static void main(String[] args) {
         NumberGameController numberGameController = new NumberGameController();
-
+        numberGameController.run();
     }
 }
