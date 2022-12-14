a8ee50e2b5ed654fe0669a258c25fba6e3bcf7ab
MINJU <alswn9938@gmail.com>
2022-12-14 22:57:26 +0900

    (Q7) add : 결과 출력

--- src/main/java/onboarding/problem7/view/OutputView.java
+++ src/main/java/onboarding/problem7/view/OutputView.java
@@ -1,5 +1,10 @@
 package onboarding.problem7.view;
 
+import onboarding.problem7.model.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
 
     public void printError(String errorMessage){
@@ -20,5 +25,17 @@ public class OutputView {
 
     public void printVisitorsInputGuide(){
         System.out.println("해당 User 방문자 목록 입력해주세요(공백으로 구분) : ");
+    }
+
+    public void printResult(List<User> result){
+        List<String> resultNames = getResultNames(result);
+        System.out.println("결과 : " + resultNames.toString());
+    }
+
+    private List<String> getResultNames(List<User> result){
+        return result.stream()
+                .map(User::getName)
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
